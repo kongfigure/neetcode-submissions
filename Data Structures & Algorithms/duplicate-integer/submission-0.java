7ebd2f3aace35824b32f4623ec48e12b7043f9e5
Nicole Kong <143675758+kongfigure@users.noreply.github.com>
2026-06-24 14:29:10 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length;j++) {
+                if (nums[j] == nums[i]) {
+                    count ++;
+                }
+            }
+            if (count > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
